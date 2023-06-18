4f562572adc885a09324b33e823aa3deca544a70
JorgeLuix <tec.jorge.29@gmai.com>
2023-06-18 15:35:19 +0200

    modifica panigation

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -12,7 +12,7 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::latest()->with('types')->paginate(5);
+        $projects = Project::latest()->with('types')->paginate(6);
         if (!$projects) {
             return response()->json([
                 'success' => false,
